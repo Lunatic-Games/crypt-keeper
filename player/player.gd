ee43698756ae6070a756bffed870604f21a51d59
JoeZlonicky <joe.zlonicky@gmail.com>
2021-06-19 15:08:45 -0700

    Select area visual

--- player/player.gd
+++ player/player.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 const SPEED = 150
 
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	var movement = Vector2()
 	if Input.is_action_pressed("move_up"):
 		movement.y -= 1
@@ -14,5 +14,16 @@ func _physics_process(_delta):
 	if Input.is_action_pressed("move_left"):
 		movement.x -= 1
 	movement = movement.normalized() * SPEED
+	
+	handle_select(delta)
 		
 	var _ret =  move_and_slide(movement)
+
+
+func handle_select(delta):
+	if Input.is_action_pressed("select"):
+		$SelectArea.visible = true
+		$SelectArea.increase(delta)
+	if Input.is_action_just_released("select"):
+		$SelectArea.visible = false
+		$SelectArea.reset()
