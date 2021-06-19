1d9328924edc9a80bb1907d0def9037e708170dc
JoeZlonicky <joe.zlonicky@gmail.com>
2021-06-19 15:28:05 -0700

    Refactored select indicator

--- player/select_area.gd
+++ player/select_area.gd
@@ -1,22 +1,27 @@
 extends Area2D
 
-const MIN_SIZE = 5.0
-const MAX_SIZE = 100.0
+const MIN_RADIUS = 5.0
+const MAX_RADIUS = 100.0
 const INCREASE_SPEED = 40.0
+const CIRCLE_POINT_COUNT = 400
+const OUTLINE_WIDTH = 2.0
 
-var radius = MIN_SIZE
+export (Color) var outline_color
+export (Color) var overlay_color
+
+var radius = MIN_RADIUS
 
 
 func _draw():
-	draw_circle(Vector2(0, 0), radius, Color(1.0, 1.0, 1.0, 0.2))
-	draw_arc(Vector2(0, 0), radius, 0, 2*PI, 400, Color(1.0, 1.0, 1.0), 2.0)
+	draw_circle(Vector2(), radius, overlay_color)
+	draw_arc(Vector2(), radius, 0, 2*PI, CIRCLE_POINT_COUNT, outline_color, OUTLINE_WIDTH)
 
 
 func increase(delta):
-	radius = min(radius + INCREASE_SPEED * delta, MAX_SIZE)
+	radius = min(radius + INCREASE_SPEED * delta, MAX_RADIUS)
 	$CollisionShape2D.shape.radius = radius
 	update()
 
 
 func reset():
-	radius = MIN_SIZE
+	radius = MIN_RADIUS
