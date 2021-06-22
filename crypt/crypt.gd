873a49358e5fbe2069a12b2dd904d733c496d080
Joe <joe.zlonicky@gmail.com>
2021-06-21 18:57:33 -0700

    Multiple waves

--- crypt/crypt.gd
+++ crypt/crypt.gd
@@ -1,29 +1,60 @@
 extends Node2D
 
 
-export (Resource) var current_wave
+export (Array, Resource) var waves
 
-const ENEMY_INDIVIDUAL_SPAWN_DELAY = 0.25
-const START_SPAWN_DELAY = 1.0
+signal wave_cleared
+
+const DELAY_BETWEEN_WAVES = 1.0
+const DELAY_BETWEEN_GROUPS = 5.0
+const DELAY_BETWEEN_ENEMIES = 0.5
 
 var spawners = []
+var current_wave: Resource
+var wave_n = 1
 
 
 func _ready():
 	randomize()
 	spawners = $Spawners.get_children()
+	update_wave_hud()
 	spawn_waves()
+	
 
 
 func spawn_waves():
-	current_wave.create_queue()
-	yield(get_tree().create_timer(START_SPAWN_DELAY), "timeout")
+	yield(get_tree().create_timer(DELAY_BETWEEN_WAVES), "timeout")
+	while waves:
+		current_wave = waves.pop_front()
+		current_wave.create_queue()
+		yield(spawn_current_wave(), "completed")
+		yield(self, "wave_cleared")
+		yield(get_tree().create_timer(DELAY_BETWEEN_WAVES), "timeout")
+		if waves:
+			wave_n += 1
+			update_wave_hud()
+
+
+func spawn_current_wave():
 	while current_wave.unit_queue:
 		var enemies = current_wave.get_next_in_queue()
 		var spawner = spawners[randi() % spawners.size()]
-		for enemy in enemies:
-			add_child(enemy)
-			enemy.global_position = spawner.global_position
-			yield(get_tree().create_timer(ENEMY_INDIVIDUAL_SPAWN_DELAY), "timeout")
-		yield(get_tree().create_timer(current_wave.time_between_groups), "timeout")
-	
+		yield(spawn_group(enemies, spawner), "completed")
+		yield(get_tree().create_timer(DELAY_BETWEEN_GROUPS), "timeout")
+
+
+func spawn_group(group, spawner):
+	for enemy in group:
+		add_child(enemy)
+		enemy.connect("tree_exited", self, "check_enemies_cleared")
+		enemy.global_position = spawner.global_position
+		yield(get_tree().create_timer(DELAY_BETWEEN_ENEMIES), "timeout")
+
+
+func update_wave_hud():
+	$HUD/Wave.text = "Wave-" + str(wave_n)
+
+
+func check_enemies_cleared():
+	if $Enemies.get_child_count() == 0:
+		emit_signal("wave_cleared")
